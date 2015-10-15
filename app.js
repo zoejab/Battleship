7e3a146641cdeeb21cabb122a5498263cc5d6a48
zoejab <zoaton@gmail.com>
2015-10-15 09:22:49 -0400

    css issue with right panel

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('battleship loaded');
+
+var Boats = function() {
+  //id
+  //location array
+  //location object
+    //spot: 8, hit = true
+  //sunk = false;
+
+  //this.sunk = function() {
+  //if all of the spots are filled then turn sunk to true;
+}
+};
